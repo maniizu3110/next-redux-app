d13be0e7d51673a9b302fb68bce3c4d42d443e5c
Leo <49385552+maniizu3110@users.noreply.github.com>
2021-05-31 04:22:03 +0900

    initFirebase動作確認

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useDispatch } from 'react-redux'
 import Link from 'next/link'
 import { startClock } from '../src/timer/actions'
 import Examples from '../components/examples'
+import firebase from '../firebase/initFirebase'
 
+firebase()
 const Index = () => {
   const dispatch = useDispatch()
   useEffect(() => {
